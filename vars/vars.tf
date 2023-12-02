2488d32e82b859bf67cc1fd3b3f62d75ac62a4c8
Nabi <nabisaheb7780@gmail.com>
2023-12-02 18:23:25 +0530

    Expense poject setup

--- vars/vars.tf
+++ vars/vars.tf
@@ -5,3 +5,11 @@ variable "sample1" {
 output "sample1" {
   value = var.sample1
 }
+
+variable "fruits" {
+  default = ["apple", "banana", "orange"]
+}
+
+output "fruits_third_count" {
+  value = var.fruits[3]
+}
