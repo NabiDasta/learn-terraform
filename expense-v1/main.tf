190446c18f8d396da14d6eaa0fc5ef1d36c3ccaf
Nabi <nabisaheb7780@gmail.com>
2023-12-08 18:36:52 +0530

    Expense poject setup

---
+++ expense-v1/main.tf
@@ -0,0 +1,50 @@
+resource "aws_instance" "frontend" {
+  ami = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = 	"sg-03097cb5888f633ea"
+  tags = {
+    Name = "frontend"
+  }
+}
+
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z0182218138ZIJRRXDHJG"
+  name    = "frontend-dev"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
+
+resource "aws_instance" "backend" {
+  ami = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = 	"sg-03097cb5888f633ea"
+  tags = {
+    Name = "backend"
+  }
+}
+
+resource "aws_route53_record" "backend" {
+  zone_id = "Z0182218138ZIJRRXDHJG"
+  name    = "backend-dev"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.backend.private_ip]
+}
+
+resource "aws_instance" "mysql" {
+  ami = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = 	"sg-03097cb5888f633ea"
+  tags = {
+    Name = "mysql"
+  }
+}
+
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z0182218138ZIJRRXDHJG"
+  name    = "mysql-dev"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
+}
